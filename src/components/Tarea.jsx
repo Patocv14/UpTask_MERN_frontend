32e5eabee63d73f6aa17f92cfb8375754f854c07
Patocv14 <PatricioCervantes140207@hotmail.com>
2022-07-17 18:23:28 -0500

    primer commit

---
+++ src/components/Tarea.jsx
@@ -0,0 +1,44 @@
+import { formatearFecha } from "../helpers/formatearFecha"
+import useProyectos from '../hooks/useProyectos'
+import useAdmin from "../hooks/useAdmin"
+
+const Tarea = ({tarea}) => {
+
+    const { handleModalEditarTarea, handleModalEliminarTarea, completarTarea } = useProyectos()
+    const admin = useAdmin()
+
+    const { descripcion, nombre, prioridad, fechaEntrega, estado, _id} = tarea
+  return (
+    <div className="border-b p-5 flex justify-between items-center">
+      <div>
+        <p className=" mb-1 text-xl">{nombre}</p>
+        <p className=" mb-1 text-sm text-gray-500 uppercase">{descripcion}</p>
+        <p className=" mb-1 text-sm">{formatearFecha(fechaEntrega)}</p>
+        <p className=" mb-1 text-xl text-gray-600">Prioridad: {prioridad}</p>
+      </div>
+      <div className="flex flex-col lg:flex-row gap-2">
+        {admin && (
+          <button
+                  className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold rounded-lg"
+                  onClick={() => handleModalEditarTarea(tarea)}
+          >Editar</button>
+        )}
+
+          <button
+            className={`${estado ? 'bg-sky-600' : 'bg-gray-600'} px-4 py-3 text-white uppercase font-bold rounded-lg`}
+            onClick={() => completarTarea(_id)}
+          >{estado ? 'Completa' : 'Incompleta '}</button>
+
+          {admin && (
+            <button
+              className="bg-red-600 px-4 py-3 text-white uppercase font-bold rounded-lg"
+              onClick={() => handleModalEliminarTarea(tarea)}
+            >Eliminar</button>
+          )}
+
+      </div>
+    </div>
+  )
+}
+
+export default Tarea
